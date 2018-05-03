d7931132165ba4eb1a6631f9fb26ead07b3fbfc8
riku.okajima <riku.okajima@gmail.com>
2018-05-03 17:40:54 +0900

    add task switcher
    modified src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 extern crate first_task;
 use first_task::tasks::*;
+use std::io::{self, Write};
 
 fn main() {
-    task1::solve();
-    task2::solve();
-    task3::solve();
-    task4::solve();
+    manage_task();
+}
+
+fn manage_task() {
+    let mut line = String::new();
+    print!("Input task number [1~7]: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut line).unwrap();
+    match line.trim_matches('\n') {
+        "1" => task1::solve(),
+        "2" => task2::solve(),
+        "3" => task3::solve(),
+        "4" => task4::solve(),
+        _ => println!("please input 1 to 5"),
+    }
 }
