06638ca1e00bec252806f464d57b050bab1f82e2
riku.okajima <riku.okajima@gmail.com>
2018-05-03 18:06:38 +0900

    add argument manage
    modified src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 extern crate first_task;
 use first_task::tasks::*;
+use std::env;
 use std::io::{self, Write};
 
 fn main() {
-    manage_task();
+    manage_task(env::args().nth(1));
 }
 
-fn manage_task() {
-    let mut line = String::new();
-    print!("Input task number [1~7]: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut line).unwrap();
-    let task_num = line.trim_matches('\n');
-    let label = format!("Task{}: ", task_num);
-    match task_num {
+fn manage_task(task: Option<String>) {
+    let (task_num, label) = match task {
+        Some(task_num) => (task_num, "".to_string()),
+        None => {
+            let mut line = String::new();
+            print!("Input task number [1~7]: ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut line).unwrap();
+            let task_num = line.trim_matches('\n');
+            let label = format!("Task{}: ", task_num);
+            (task_num.to_string(), label)
+        }
+    };
+    match task_num.as_str() {
         "1" => task1::solve(&label),
         "2" => task2::solve(&label),
         "3" => task3::solve(&label),
